2bdae53ca65b14ecb284ce8abf16c74088e1b8a0
gosp <141959@qq.com>
2020-05-29 14:18:04 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 mkdir -p $PWD/.cache
-echo $*
+
 /g11nradar/bin/g11n-radar -p $PWD -c $PWD/.cache -d $PWD/detail.csv -s $PWD/summary.csv rule --nostring --locale $*
+
+number=`awk -F',' 'FNR==2 {print $4}' summary.csv`
+ 
+ 
+if [ "${number}" -gt "0" ]; then
+  echo "G11n Issues are found!"
+  exit 1
+fi
